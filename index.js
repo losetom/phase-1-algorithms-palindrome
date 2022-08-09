5e0f0678d01e1de89debfd9709c4a5b02fd56628
Mike Loseto <losetom1998@gmail.com>
2022-08-09 15:15:37 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  return word == word.split('').reverse().join('')
 }
 
 /* 
   Add your pseudocode here
+  
+function isPalindrome(word) {
+  return word == word.split('').reverse().join('')
+}
+
 */
 
 /*
   Add written explanation of your solution here
+
+  any string passed as an argument will be placed into an array like object
+  the array like object will be reversed to check it is a palindrome or not 
+  join() will join the array like objects to be checked  
 */
 
 // You can run `node index.js` to view these console logs
